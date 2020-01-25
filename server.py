d3b3dbed9a54fec765552159ccb1df2a7c0da16b
Your Name <you@example.com>
2020-01-24 17:02:44 -0800

    added API keys

--- server.py
+++ server.py
@@ -37,7 +37,13 @@ def find_afterparties():
     sort = request.args.get('sort', '')
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
-    payload = {'apikey': API_KEY}
+    payload = {'apikey': API_KEY,
+               'keyword': keyword,
+               'postalCode': postalcode,
+               'radius': radius,
+               'unit': unit,
+               'sort': sort
+               }
 
     # TODO: Make a request to the Event Search endpoint to search for events
     #
@@ -50,8 +56,10 @@ def find_afterparties():
     # - Replace the empty list in `events` with the list of events from your
     #   search results
 
-    data = {'Test': ['This is just some test data']}
-    events = []
+    response = requests.get(url, params=payload)
+    data = response.json()
+    print(data)
+    events = data['_embedded']['events']
 
     return render_template('search-results.html',
                            pformat=pformat,
